c069f315b0dbdfd7a933c2c748f5b7b11614a714
Atul Gupta <atul.iseet@yahoo.co.in>
2017-12-28 22:16:38 +0530

    Adds Form

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+#app.config['SECRET_KEY'] = 'you-will-never-guess' # or below
+app.config.from_object('config.Config')
+
 from app import views
