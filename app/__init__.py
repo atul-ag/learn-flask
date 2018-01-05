c74c6a7dbbc1533a4f40b36649e0320b9a850398
Atul Gupta <atul.iseet@yahoo.co.in>
2018-01-05 15:36:44 +0530

    Adds Login and Register working

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 #app.config['SECRET_KEY'] = 'you-will-never-guess' # or below
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import views, models
