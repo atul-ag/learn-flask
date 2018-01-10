0ac618fe9f29990947e2c3caf8a28fc099194baf
Ubuntu <ubuntu@ip-172-31-28-83.us-west-2.compute.internal>
2018-01-10 13:27:37 +0000

    From EC2

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 from app import app, db
 from app.models import User, Post
-
+import os
 @app.shell_context_processor
 def make_shell_context():
 	return {'db': db, 'User': User, 'Post': Post}
 
-app.run(debug=True)
+app.run(
+	host=os.getenv('LISTEN', '0.0.0.0'),
+	port=int(os.getenv('PORT', '8080')),
+	debug=True
+)
+
+#app.run(debug=True)
 
