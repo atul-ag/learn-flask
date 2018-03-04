921825af908759ac280de405690514b9dbaaa478
Atul <atul.iseet@yahoo.co.in>
2018-03-04 20:23:28 +0530

    reset although connect() error

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import SMTPHandler
+from flask_mail import Mail
 import os
 
 app = Flask(__name__)
@@ -13,7 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-
+mail = Mail(app)
 from app import views, models, errors
 
 if not app.debug:
